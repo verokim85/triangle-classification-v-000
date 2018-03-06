77ca4662ed143eac7242d82865c05b62d074c970
Veronica Kim <vero.a.kim@gmail.com>
2018-03-06 00:33:12 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,10 +7,16 @@ class Triangle
   end
 
   def kind
-    if @side_one == @side_two && @side_two == @side_three
+    if @side_one == @side_two && @side_two ==   
+      @side_three
       return :equilateral
-    elsif @side_one == @side_two && @side_three != @side_one
+    elsif @side_one == @side_two && @side_three !=      
+      @side_one
       return :isosceles
+    elsif @side_one != @side_two && @side_two != 
+      @side_three
+      return :scalene
+    
     end
   end
 
