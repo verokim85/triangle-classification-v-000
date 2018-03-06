add2fb8c5363b41df051b324da04ad131118de55
Veronica Kim <vero.a.kim@gmail.com>
2018-03-06 00:17:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
-  # write code here
+
+  attr_accessor
+
+  def initialize(side_one, side_two, side_three)
+
+    end
+
+  def kind
+  end
+
+end
+
+class TriangleError < StandardError
+
 end
