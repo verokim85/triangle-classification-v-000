a9a16ac4653ed47aefc0e861e1f6932caad293e3
Veronica Kim <vero.a.kim@gmail.com>
2018-03-06 00:18:24 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,9 @@ class Triangle
   attr_accessor
 
   def initialize(side_one, side_two, side_three)
-
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
     end
 
   def kind
